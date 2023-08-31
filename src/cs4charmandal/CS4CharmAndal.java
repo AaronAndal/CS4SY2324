ea81d10aba2c00bdfaa4aec81245ff0eb9113443
AaronAndal <112668479+AaronAndal@users.noreply.github.com>
2023-08-31 15:29:56 +0800

    Update CS4CharmAndal.java
    Not Final

--- src/cs4charmandal/CS4CharmAndal.java
+++ src/cs4charmandal/CS4CharmAndal.java
@@ -10,15 +10,63 @@ package cs4charmandal;
  * @author CHARM
  */
 public class CS4CharmAndal {
-    int sillyLevel =  
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
-        Main myObj = new Main();
+        
+        String name1 = "Cheeseburger Pringles";
+        int rating1 = 1;
+        int caloriesPerServing1 = 150;
+        String ratingComparison1;
+        
+        String name2 = "Sour Cream Pringles";
+        int rating2 = 9;
+        int caloriesPerServing2 = 150;
+        String ratingComparison2;
+        
+        String name3 = "BBQ Pringles";
+        int rating3 = rating1 + rating2;
+        int caloriesPerServing3 = 150;
+        String ratingComparison3;
+
+        int meanRating = (rating1 + rating2 + rating3) / 3;
+
+        if (rating1 > meanRating) {
+            ratingComparison1 = "Yes";
+        } else {
+            ratingComparison1 = "No";
+        }
+
+        if (rating2 > meanRating) {
+            ratingComparison2 = "Yes";
+        } else {
+            ratingComparison2 = "No";
+        }
+
+        if (rating3 > meanRating) {
+            ratingComparison3 = "Yes";
+        } else {
+            ratingComparison3 = "No";
+        }
+        
+        System.out.println("Flavor #1: " + name1);
+        System.out.println( "    " + "Flavor Rating (Out of 10): " + rating1);
+        System.out.println( "    " + "Calories (per Serving): " + caloriesPerServing1);
+        System.out.println( "    " + "Is the rating higher than the mean rating" + '(' + meanRating + ")?: " + ratingComparison1);
+        System.out.println();
+        
+        System.out.println("Flavor #2: " + name2);
+        System.out.println( "    " + "Flavor Rating (Out of 10): " + rating2);
+        System.out.println( "    " + "Calories (per Serving): " + caloriesPerServing2);
+        System.out.println( "    " + "Is the rating higher than the mean rating" + '(' + meanRating + ")?: " + ratingComparison2);
+        System.out.println();
+        
+        System.out.println("Flavor #3: " + name3);
+        System.out.println( "    " + "Flavor Rating (Out of 10): " + rating3 + "// (Operation: sum)");
+        System.out.println( "    " + "Calories (per Serving): " + caloriesPerServing3);
+        System.out.println( "    " + "Is the rating higher than the mean rating" + '(' + meanRating + ")?: " + ratingComparison3);
     }
     
 }
