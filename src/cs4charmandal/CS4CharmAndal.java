74254bf4e51481acdef67c23570cd30d2597d16b
AaronAndal <112668479+AaronAndal@users.noreply.github.com>
2023-09-08 21:11:17 +0800

    Update CS4CharmAndal.java
    final update: minor changes and additions for final submission

--- src/cs4charmandal/CS4CharmAndal.java
+++ src/cs4charmandal/CS4CharmAndal.java
@@ -19,19 +19,23 @@ public class CS4CharmAndal {
         String name1 = "Cheeseburger Pringles";
         int rating1 = 1;
         int caloriesPerServing1 = 150;
+        String limitedEdition1 = "True";
         String ratingComparison1;
         
         String name2 = "Sour Cream Pringles";
         int rating2 = 9;
         int caloriesPerServing2 = 150;
+        String limitedEdition2 = "False";
         String ratingComparison2;
         
         String name3 = "BBQ Pringles";
         int rating3 = 10;
         int caloriesPerServing3 = 150;
+        String limitedEdition3 = "False";
         String ratingComparison3;
 
         int meanRating = (rating1 + rating2 + rating3) / 3;
+        int totalRating = rating1 + rating2 + rating3;
 
         if (rating1 > meanRating) {
             ratingComparison1 = "Yes";
@@ -54,18 +58,21 @@ public class CS4CharmAndal {
         System.out.println("Flavor #1: " + name1);
         System.out.println( "    " + "Flavor Rating (Out of 10): " + rating1);
         System.out.println( "    " + "Calories (per Serving): " + caloriesPerServing1);
+        System.out.println( "    " + "Limited Edition: " + limitedEdition1);
         System.out.println( "    " + "Is the rating higher than the mean rating" + '(' + meanRating + ")?: " + ratingComparison1);
         System.out.println();
         
         System.out.println("Flavor #2: " + name2);
         System.out.println( "    " + "Flavor Rating (Out of 10): " + rating2);
         System.out.println( "    " + "Calories (per Serving): " + caloriesPerServing2);
+        System.out.println( "    " + "Limited Edition: " + limitedEdition2);
         System.out.println( "    " + "Is the rating higher than the mean rating" + '(' + meanRating + ")?: " + ratingComparison2);
         System.out.println();
         
         System.out.println("Flavor #3: " + name3);
         System.out.println( "    " + "Flavor Rating (Out of 10): " + rating3);
         System.out.println( "    " + "Calories (per Serving): " + caloriesPerServing3);
+        System.out.println( "    " + "Limited Edition: " + limitedEdition3);
         System.out.println( "    " + "Is the rating higher than the mean rating" + '(' + meanRating + ")?: " + ratingComparison3);
         System.out.println();
         
@@ -76,6 +83,8 @@ public class CS4CharmAndal {
         else {
             System.out.println("All ratings are equal: False");
         }
+        System.out.println("Total Rating: " + totalRating);
+        
     }
     
 }
